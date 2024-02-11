1fd6702b877b4f7180bcbab2207ec47599851142
debojit <debojitganguly69@gmail.com>
2024-02-11 18:15:26 +0530

    Added the graph successfully

--- src/App.jsx
+++ src/App.jsx
@@ -34,9 +34,9 @@ function App() {
         {
           selector: 'node',
           style: {
-            "border-width": '5px',
-            'width': '50px',
-            'height': '50px',
+            "border-width": '2px',
+            'width': '40px',
+            'height': '40px',
             'text-valign': 'center',
             'text-halign': 'center',
             'border-color': 'black',
@@ -49,7 +49,7 @@ function App() {
         {
           selector: 'edge',
           style: {
-            'width': 4,
+            'width': 2,
             'line-color': 'black',
             'target-arrow-color': 'black',
             'target-arrow-shape': 'triangle',
@@ -67,13 +67,12 @@ function App() {
   function gen() {
     cy.elements().remove();
     const arr = [];
-    const val = document.querySelector(".input1")
+    const val = document.querySelector(".node_number")
     const n = val.value;
     for (var i = 1; i <= n; i++) {
       arr.push(
         {
           data: { id: `${i}` },
-          // position: { x: Math.random() * (1000 - 20) + 20, y: Math.random() * (200 - 25) + 40 }
 
         }
       )
@@ -82,27 +81,30 @@ function App() {
     cy.layout(options).run();
   }
   function add_edge() {
-    const edge1 = document.querySelector(".edge1").value.split('\n');
-    const edge2 = document.querySelector(".edge2").value.split('\n');
+    const edge1 = document.querySelector(".edge_specification").value.split('\n');
     for (var i = 0; i < edge1.length; i++) {
       cy.add([
         {
-          data: { source: `${edge1[i]}`, target: `${edge2[i]}` }
+          data: { source: `${edge1[i].split(' ')[0]}`, target: `${edge1[i].split(' ')[1]}` }
         }
       ])
     }
   }
   return (
     <>
-      <div className="cy-container">
-        <div id="cy"></div>
-      </div>
-      <div>
-        <input type="number" className="input1"></input>
-        <button onClick={gen} className="generate">genenrate</button>
-        <textarea type="number" className="edge1"></textarea>
-        <textarea type="number" className="edge2"></textarea>
-        <button onClick={add_edge} className="add_edge">Add Edge</button>
+      <div className="graph-interface">
+        <div className="nodes_input">
+          <label>Number Of Nodes</label>
+          <input onChange={gen} type="text" className="node_number"></input>
+          <div className="edge_input">
+            <label>Edges</label>
+            <textarea className="edge_specification"></textarea>
+            <button onClick={add_edge} className="add_edge">Add Edge</button>
+          </div>
+        </div>
+        <div className="cy-container">
+          <div id="cy"></div>
+        </div>
       </div>
     </>
   )
