d25bbd8571dba9d14957085ccf4f3e391dcf7e9e
debojit <debojitganguly69@gmail.com>
2024-02-18 08:45:19 +0530

    Added Featuring of Drawing

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,33 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import cytoscape from 'cytoscape';
+import edgehandles from 'cytoscape-edgehandles';
 
+cytoscape.use(edgehandles);
 function App() {
   const [cy, setcy] = useState([]);
+  const [drawedge, setdrawedge] = useState({});
   let options = {
     name: 'circle',
-
-    fit: true, // whether to fit the viewport to the graph
-    padding: 30, // the padding on fit
-    boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
-    avoidOverlap: true, // prevents node overlap, may overflow boundingBox and radius if not enough space
-    nodeDimensionsIncludeLabels: false, // Excludes the label when calculating node bounding boxes for the layout algorithm
-    spacingFactor: undefined, // Applies a multiplicative factor (>0) to expand or compress the overall area that the nodes take up
-    radius: undefined, // the radius of the circle
-    startAngle: 3 / 2 * Math.PI, // where nodes start in radians
-    sweep: undefined, // how many radians should be between the first and last node (defaults to full circle)
-    clockwise: true, // whether the layout should go clockwise (true) or counterclockwise/anticlockwise (false)
-    sort: undefined, // a sorting function to order the nodes; e.g. function(a, b){ return a.data('weight') - b.data('weight') }
-    animate: false, // whether to transition the node positions
-    animationDuration: 500, // duration of animation in ms if enabled
-    animationEasing: false, // easing of animation if enabled
-    animateFilter: function (node, i) { return true; }, // a function that determines whether the node should be animated.  All nodes animated by default on animate enabled.  Non-animated nodes are positioned immediately when the layout starts
-    ready: undefined, // callback on layoutready
-    stop: undefined, // callback on layoutstop
-    transform: function (node, position) { return position; } // transform a given node position. Useful for changing flow direction in discrete layouts 
-
+    fit: true,
+    randomize: true
   };
   useEffect(() => {
-    const cy1 = cytoscape({
+    let cy1 = cytoscape({
       container: document.getElementById('cy'),
       style: [
         {
           selector: 'node',
           style: {
             "border-width": '2px',
-            'width': '40px',
-            'height': '40px',
+            'width': '35px',
+            'height': '35px',
             'text-valign': 'center',
             'text-halign': 'center',
             'border-color': 'black',
             'background-color': 'white',
             'content': 'data(id)',
-            'font-size': '20px',
+            'font-size': '17px',
           }
         },
 
@@ -58,14 +43,15 @@ function App() {
         }
       ],
       zoomingEnabled: false,
-      autounselectify: true,
     });
+    setdrawedge(cy1.edgehandles());
     setcy(cy1);
   }, [])
 
-
+  // var maow = cy.edgehandles();
   function gen() {
     cy.elements().remove();
+    drawedge.enableDrawMode();
     const arr = [];
     const val = document.querySelector(".node_number")
     const n = val.value;
@@ -93,14 +79,10 @@ function App() {
   }
   function download_graph_png() {
     const canvas = document.querySelector('canvas[data-id="layer2-node"]');
-    // console.log(canvas);
     const link = document.createElement('a');
     const dataURL = canvas.toDataURL('image/png');
-    // console.log(dataURL);
     link.href = dataURL;
-    // console.log(link.href);
     link.download = 'graph.png';
-    // console.log(link.download);
     link.click();
   }
   return (
@@ -119,6 +101,7 @@ function App() {
           </div>
         </div>
         <div className="cy-container">
+          <p>Graph Visualization</p>
           <div id="cy"></div>
         </div>
       </div>
